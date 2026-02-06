8f3d5da2274df2d2fd5d303bca6d40cbc98480f5
seria <seria.ati@gmail.com>
2026-02-06 16:41:55 +0900

    feat: Allow changing host and port through env var

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import os
+
 import uvicorn
+from dotenv import load_dotenv
 
 import hun
 
 if __name__ == "__main__":
-    uvicorn.run(hun.app, port=8092)
+    load_dotenv()
+    uvicorn.run(hun.app, port=int(os.getenv("PORT", "8092")), host=os.getenv("HOST", "127.0.0.1"))
