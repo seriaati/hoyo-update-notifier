666e4156cd767a11b008be3638cf6f4c8cf20161
studiobutter <bebosua05@yahoo.com.vn>
2026-03-04 01:31:18 +0700

    fix: Add check to create a database directory (data) before initalizing the service to prevent database lockup due to no database directory.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from pathlib import Path
+from colorama import Fore, Style, init
 
 import os
 
@@ -6,6 +8,18 @@ import uvicorn
 from dotenv import load_dotenv
 
 import hun
+
+init(autoreset=True)
+
+def info(msg: str):
+    print(f"{Fore.GREEN}INFO:{Style.RESET_ALL} {msg}")
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+
+if not DATA_DIR.exists():
+    info(f"Creating data directory at {DATA_DIR}")
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 if __name__ == "__main__":
     load_dotenv()
